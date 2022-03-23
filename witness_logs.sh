a98b153e55e9cf97f8dbff38eafc9b699d6f8309
Colin <7887972+cln-io@users.noreply.github.com>
2022-03-23 16:10:21 +0100

    direct directory

--- witness_logs.sh
+++ witness_logs.sh
@@ -11,6 +11,6 @@
 # 2022-01-26 17:03:43.682 Max retry
 
 #process everything in the log as of now:
-sudo docker exec miner cat     /var/data/log/console.log | grep --text witness |  awk '{if ($6=="successfully") print "\x1b[32m"$1" "$2" Successfully sent \x1b[0m"; else if ($13=="treating") print $1" "$2" Heard witness"; else if ($10=="max") print "\x1b[31m"$1" "$2" Max retry\x1b[0m"; }'
+cat     /opt/helium/miner_data/log/console.log | grep --text witness |  awk '{if ($6=="successfully") print "\x1b[32m"$1" "$2" Successfully sent \x1b[0m"; else if ($13=="treating") print $1" "$2" Heard witness"; else if ($10=="max") print "\x1b[31m"$1" "$2" Max retry\x1b[0m"; }'
 #tail the log
-sudo docker exec miner tail -f /var/data/log/console.log | grep --text witness |  awk '{if ($6=="successfully") print "\x1b[32m"$1" "$2" Successfully sent \x1b[0m"; else if ($13=="treating") print $1" "$2" Heard witness"; else if ($10=="max") print "\x1b[31m"$1" "$2" Max retry\x1b[0m"; }'
+tail -f /opt/helium/miner_data/log/console.log | grep --text witness |  awk '{if ($6=="successfully") print "\x1b[32m"$1" "$2" Successfully sent \x1b[0m"; else if ($13=="treating") print $1" "$2" Heard witness"; else if ($10=="max") print "\x1b[31m"$1" "$2" Max retry\x1b[0m"; }'
