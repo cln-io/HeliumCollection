f8bd58ecdf723a4ce476d3a308f86b970f3b0673
Colin <7887972+cln-io@users.noreply.github.com>
2022-03-23 19:29:12 +0100

    Colours

--- get-height.sh
+++ get-height.sh
@@ -2,12 +2,22 @@
 #original file from https://github.com/Doginal/helium-network-scripts/blob/4a4c2b0b2f0df650cd0bbd208ddcedb0c915b568/get-height.sh
 #modified to grab the docker name so no input is required
 #https://stackoverflow.com/questions/16616975/how-do-i-get-the-last-word-in-each-line-with-bash
+#colors -> https://gist.github.com/abritinthebay/d80eb99b2726c83feb0d97eab95206c4
 
 DOCKER_NAME=$(docker ps | grep miner:miner | awk 'NF>1{print $NF}')
 CURRENT=$(($(curl --silent https://api.helium.io/v1/blocks/height -q | jq '.data.height')+0 ))
 MINER=$(($(docker exec $DOCKER_NAME miner info height | cut -f 3)+0 ))
 BEHIND=$(( $CURRENT - $MINER ))
-PBEHIND=$(bc <<< "scale=2; $MINER/$CURRENT*100")
-echo "Behind by: $BEHIND blocks"
-echo "Miner: $MINER ($PBEHIND%)"
-echo "Current Height: $CURRENT"
+PBEHIND=$(bc <<< "scale=4; $MINER/$CURRENT*100")
+if [ $BEHIND -le 0 ];
+then
+        printf "\x1b[32mIn sync (or ahead): %i blocks\x1b[0m\n" $BEHIND
+elif [[ $BEHIND -gt 0 && $BEHIND -le 20 ]];
+then
+        printf "\x1b[33mBehind by: %i block(s)\x1b[0m\n" $BEHIND
+elif [ $BEHIND -gt 20 ];
+then
+        printf "\x1b[31mBehind by: %i block(s)\x1b[0m\n" $BEHIND
+fi
+printf "Miner: %i (%.2f%%)\n" $MINER $PBEHIND
+printf "Helium Network Height: %i\n" $CURRENT
